46648bc182d91ac5d0b09b3f800201316f57332c
BajcarM <102542768+BajcarM@users.noreply.github.com>
2022-04-29 13:38:19 +0200

    added all figures and their moves, selecting behavior and some styling

--- index.js
+++ index.js
@@ -1,53 +1,149 @@
 const gameboard = document.querySelector(".gameboard");
-const movesArrayHorse = [
-  ["+2", "+1"],
-  ["+2", "-1"],
-  ["+1", "+2"],
-  ["-1", "+2"],
-  ["-2", "+1"],
-  ["-2", "-1"],
-  ["-1", "-2"],
-  ["+1", "-2"],
+const figuresContainer = document.querySelector(".figures-container");
+
+const figures = [
+  {
+    name: "king",
+    moves: function () {
+      const king = [];
+      for (i = -1; i < 2; i++) {
+        for (j = -1; j < 2; j++) {
+          king.push([i, j]);
+        }
+      }
+
+      return king;
+    },
+  },
+  {
+    name: "knight",
+    moves: function () {
+      const knight = [];
+      for (i = -1; i < 2; i += 2) {
+        for (j = -1; j < 2; j += 2) {
+          knight.push([2 * j, i], [i, 2 * j]);
+        }
+      }
+
+      return knight;
+    },
+  },
+  {
+    name: "pawn",
+    moves: function () {
+      return [["-1", "0"]];
+    },
+  },
+  {
+    name: "rook",
+    moves: function () {
+      const rook = [];
+      for (i = -7; i < 8; i++) {
+        rook.push(["0", i], [i, "0"]);
+      }
+      return rook;
+    },
+  },
+  {
+    name: "bishop",
+    moves: function () {
+      const bishop = [];
+      for (i = -7; i < 8; i++) {
+        bishop.push([i, i], [i, -i]);
+      }
+      return bishop;
+    },
+  },
+  {
+    name: "queen",
+    moves: function () {
+      const queen = [];
+      for (i = -7; i < 8; i++) {
+        queen.push([i, i], [i, -i], ["0", i], [i, "0"]);
+      }
+      return queen;
+    },
+  },
 ];
 
-let selectedFigure = movesArrayHorse;
+let selectedFigure = null;
 let position = "";
 
 createGameboard(8, 8);
+createFigures();
 
 gameboard.addEventListener("click", (e) => {
+  if (!selectedFigure) {
+    document.querySelector(".modal").style.display = "grid";
+    setTimeout(() => {
+      document.querySelector(".modal").style.display = "none";
+    }, 2000);
+    return;
+  }
   position = e.target.dataset.coords;
-  gameboard
-    .querySelectorAll(".tile")
-    .forEach((tile) => (tile.textContent = ""));
+  gameboard.querySelectorAll(".tile").forEach((tile) => {
+    tile.textContent = "";
+    tile.classList.remove(`tile-selected`, `figure-${selectedFigure.name}`);
+  });
+
   e.target.textContent = "0";
-  checkPossibleMoves(position, selectedFigure);
+  e.target.classList.add(`tile-selected`, `figure-${selectedFigure.name}`);
+
+  checkPossibleMoves(position, selectedFigure.moves());
+});
+
+figuresContainer.addEventListener("click", (e) => {
+  const closestFigureContainer = e.target.closest(".figure-container");
+  gameboard.querySelectorAll(".tile").forEach((tile) => {
+    tile.textContent = "";
+    if (selectedFigure) {
+      tile.classList.remove(`figure-selected`, `figure-${selectedFigure.name}`);
+    }
+  });
+  if (closestFigureContainer.classList.contains("figure-selected")) {
+    closestFigureContainer.classList.remove("figure-selected");
+    selectedFigure = null;
+  } else {
+    figuresContainer
+      .querySelectorAll(".figure-container")
+      .forEach((element) => {
+        element.classList.remove("figure-selected");
+      });
+
+    closestFigureContainer.classList.add("figure-selected");
+    selectedFigure = figures[closestFigureContainer.dataset.index];
+  }
 });
 
 function createGameboard(rows, cols) {
-  // Create a board
-
   gameboard.innerHTML = "";
   let tiles = [];
+  let tileColors = ["white", "black"];
   for (i = 0; i < rows; i++) {
     for (j = 0; j < cols; j++) {
       tiles.push(
-        `<div class="tile tile-empty tile-black" data-coords="${i}${j}"></div>`
+        `<div class="tile tile-empty tile-${
+          tileColors[(i + j) % 2]
+        }" data-coords="${i}${j}"></div>`
       );
     }
   }
   gameboard.innerHTML = tiles.join("");
+}
 
-  // Color tiles
+function createFigures() {
+  let figuresArray = figures.reduce((acc, item, index) => {
+    acc.push(
+      `<div class="figure-container" data-index="${index}">
+            <div class="figure figure-${item.name}"></div>
+            <p>${item.name}</p>
+        </div>`
+    );
+    return acc;
+  }, []);
 
-  document.querySelectorAll(".tile").forEach((tile) => {
-    const row = parseInt(tile.dataset.coords[0]);
-    const col = parseInt(tile.dataset.coords[1]);
-    if ((row % 2 && col % 2) || (!(row % 2) && !(col % 2))) {
-      tile.classList.remove("tile-black");
-      tile.classList.add("tile-white");
-    }
-  });
+  document.querySelector(".figures-container").innerHTML =
+    figuresArray.join("");
 }
 
 function checkPossibleMoves(coords, movesArray) {
@@ -80,4 +176,3 @@ function checkPossibleMoves(coords, movesArray) {
     stepCount++;
   } while (currentStep.length > 0);
 }
-
